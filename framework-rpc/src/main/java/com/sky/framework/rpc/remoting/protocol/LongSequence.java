00bd3d86d8236f10b4eba146c6404982df86c629
shen11961677@163.com <shenliwei521>
2019-08-14 10:32:47 +0800

    init

--- framework-rpc/src/main/java/com/sky/framework/rpc/remoting/protocol/LongSequence.java
+++ framework-rpc/src/main/java/com/sky/framework/rpc/remoting/protocol/LongSequence.java
@@ -33,11 +33,8 @@ class LongRhsPadding extends LongValue {
 /**
  * 序号生成器, 每个线程预先申请一个区间, 步长(step)固定, 以此种方式尽量减少CAS操作,
  * 需要注意的是, 这个序号生成器不是严格自增的, 并且也溢出也是可以接受的(接受负数).
- * <p>
- * jupiter
- * org.jupiter.common.util
  *
- * @author jiachun.fjc
+ * @author
  */
 public class LongSequence extends LongRhsPadding {
 
