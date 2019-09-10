ca285a2cad3f7b5073c306e33c37a9c1d66b6031
shen11961677@163.com <shen11961677@163.com>
2019-09-10 11:02:28 +0800

    fix(doc): 修复重构目录后swagger无效问题
    修复重构目录后swagger无效问题

--- framework-integrate/framework-integrate-swagger/src/main/java/com/sky/framework/swagger/SwaggerAutoConfiguration.java
+++ framework-integrate/framework-integrate-swagger/src/main/java/com/sky/framework/swagger/SwaggerAutoConfiguration.java
@@ -25,7 +25,9 @@ package com.sky.framework.swagger;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -50,10 +52,12 @@ import java.util.List;
  * @author
  */
 @Configuration
+@EnableConfigurationProperties(value = SwaggerProperties.class)
+@ComponentScan(basePackageClasses = SwaggerAutoConfiguration.class)
 @ConditionalOnProperty(value = SwaggerProperties.SWAGGER_ENABLE, matchIfMissing = true)
 @EnableSwagger2
 @Slf4j
-public class SwaggerConfiguration {
+public class SwaggerAutoConfiguration {
 
     @Resource
     private SwaggerProperties swaggerProperties;
