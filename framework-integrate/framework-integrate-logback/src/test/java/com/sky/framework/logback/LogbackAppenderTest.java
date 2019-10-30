d7b79c1d94a92b17ae3c567036a107817db46ebf
shen11961677@163.com <shen11961677@163.com>
2019-10-30 13:21:04 +0800

    feat(logback): logback模块增加实现
    logback模块增加实现

---
+++ framework-integrate/framework-integrate-logback/src/test/java/com/sky/framework/logback/LogbackAppenderTest.java
@@ -0,0 +1,49 @@
+/*
+ * The MIT License (MIT)
+ * Copyright © 2019 <sky>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.sky.framework.logback;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+/**
+ * @author
+ */
+public class LogbackAppenderTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogbackAppenderTest.class);
+
+    @Test
+    public void test() {
+        MDC.put("MDC_KEY", "MDC_VALUE");
+        MDC.put("THREAD_ID", String.valueOf(Thread.currentThread().getId()));
+
+        LOGGER.trace("trace log");
+        LOGGER.debug("debug log");
+        LOGGER.info("info log");
+        LOGGER.warn("warn log");
+        LOGGER.error("error log");
+    }
+
+}
