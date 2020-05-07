b922e42f9e457239bb62bfa024a2724a0a6d40aa
沈利伟 <liwei.shen@baozun.com>
2020-05-07 17:20:49 +0800

    feat(*): 增加通知模块(企业微信或钉钉)
    增加通知模块(企业微信或钉钉)

---
+++ framework-util/framework-util-notify/src/main/java/com/framework/util/notify/wx/message/MarkdownMessage.java
@@ -0,0 +1,117 @@
+/*
+ * The MIT License (MIT)
+ * Copyright © 2019-2020 <sky>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.framework.util.notify.wx.message;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author
+ */
+public class MarkdownMessage implements Message {
+
+    private List<String> items = new ArrayList<>();
+
+    public void add(String text) {
+        items.add(text);
+    }
+
+    public static String getBoldText(String text) {
+        return "**" + text + "**";
+    }
+
+    public static String getItalicText(String text) {
+        return "*" + text + "*";
+    }
+
+    public static String getLinkText(String text, String href) {
+        return "[" + text + "](" + href + ")";
+    }
+
+    public static String getImageText(String imageUrl) {
+        return "![image](" + imageUrl + ")";
+    }
+
+    public static String getHeaderText(int headerType, String text) {
+        if (headerType < 1 || headerType > 6) {
+            throw new IllegalArgumentException("headerType should be in [1, 6]");
+        }
+
+        StringBuilder numbers = new StringBuilder();
+        for (int i = 0; i < headerType; i++) {
+            numbers.append("#");
+        }
+        return numbers + " " + text;
+    }
+
+    public static String getReferenceText(String text) {
+        return "> " + text;
+    }
+
+    public static String getOrderListText(List<String> orderItem) {
+        if (orderItem.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= orderItem.size() - 1; i++) {
+            sb.append(String.valueOf(i) + ". " + orderItem.get(i - 1) + "\n");
+        }
+        sb.append(String.valueOf(orderItem.size()) + ". " + orderItem.get(orderItem.size() - 1));
+        return sb.toString();
+    }
+
+    public static String getUnorderListText(List<String> unorderItem) {
+        if (unorderItem.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < unorderItem.size() - 1; i++) {
+            sb.append("- " + unorderItem.get(i) + "\n");
+        }
+        sb.append("- " + unorderItem.get(unorderItem.size() - 1));
+        return sb.toString();
+    }
+
+    @Override
+    public String toJsonString() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("msgtype", "markdown");
+
+        Map<String, Object> markdown = new HashMap<>();
+
+        StringBuilder markdownText = new StringBuilder();
+        for (String item : items) {
+            markdownText.append(item + "\n");
+        }
+        markdown.put("content", markdownText.toString());
+        result.put("markdown", markdown);
+
+        return JSON.toJSONString(result);
+    }
+}
